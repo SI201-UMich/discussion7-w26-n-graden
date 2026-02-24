3f804745e67a705782db9e6c2950fbab9d9dfa9e
n-graden <ngraden@umich.edu>
2026-02-24 15:27:06 -0500

    Did portion of task 2

--- discussion7.py
+++ discussion7.py
@@ -62,8 +62,16 @@ def calculate_avg_price_by_neighbourhood_group_and_room(listings):
         dict mapping (neighbourhood_group, room_type) -> average_price (float)
         e.g. { ('Downtown', 'Entire home/apt'): 123.45, ... }
     """
-    avg = 0
-    
+    price_sums = {}
+    price_counts = {}
+
+    for listing in listings:
+        neighbourhood_group = listing['neighbourhood_group']
+        room_type = listing['room_type']
+        price = float(listing['price'])
+
+        key = (neighbourhood_group, room_type)
+        
 
 
 
